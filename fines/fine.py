6b3264afbd593baace6fd39a94a46b74eebbc533
Danny Aldham <daniel.aldham@gmail.com>
2022-10-18 23:34:29 +0200

    update auth fix, and index.html introduced

--- fines/fine.py
+++ fines/fine.py
@@ -8,3 +8,40 @@ from fines.auth import login_required
 from fines.db import get_db
 
 bp = Blueprint('fine', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+
+    return render_template('fine/index.html', posts=posts)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('fine.index'))
+
+    return render_template('fine/create.html')
+
