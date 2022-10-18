fb928f2e2afd4c56560b2f118fc2e0140520dac1
Daniel Aldham <daniel.aldham@gmail.com>
2022-10-18 15:05:11 +0200

    update init file and new gitignore file

--- fines/__init__.py
+++ fines/__init__.py
@@ -8,3 +8,19 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'fines.sqlite'),
     )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    return app
